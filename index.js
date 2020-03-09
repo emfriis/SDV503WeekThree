a4a09e6cba47093f8ecb715751f50223543514bd
Emily Friis <emily-friis@live.nmit.ac.nz>
2020-03-09 13:54:03 +1300

    Added numbers as strings and strings as numbers Code

--- index.js
+++ index.js
@@ -24,3 +24,16 @@ console.log('Post-Increment for numFirst:');
 console.log(numFirst);
 console.log('Post-Increment for numSecond:');
 console.log(numSecond);
+
+let numAsString = numFirst + stringNumFirst
+console.log("Evaluation of numeric value 3 + string value '6':");
+console.log(numAsString);
+console.log("Numeric value 3 plus String value '6' evaluates to a concatenated string with the value '36'");
+
+let stringNumSecond = '10';
+let stringNumThird = '12';
+
+let stringsToNums = +stringNumSecond + +stringNumThird;
+console.log("Evaluation of string '10' plus '12' converted to number values:");
+console.log(stringsToNums);
+console.log("Evaluates to result of numeral operator with strings converted to numeric values");
